7ff67a777e67dbd6b95645bdafab289ab0f8c800
Nhora <118848679+Nhoni@users.noreply.github.com>
2023-10-31 14:39:02 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react"; //Librairie
+import ReactDOM from "react-dom/client"; //Librairie
+import App from "./App.js"; //Composant (Component)
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
